eba824b99d63a7d8bc7add883c717e9126273e04
SimonCao1207 <hainam@kaist.ac.kr>
2024-01-30 15:29:47 +0900

    faster

--- sliding_window/minimumSizesubarraySum.cpp
+++ sliding_window/minimumSizesubarraySum.cpp
@@ -14,42 +14,29 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int ans=0, _size=1;
-        int start=0, end=0, _sum=0;
-        while (_sum < target && end < nums.size()){
-            _sum += nums[end];
-            end++;
-        }
-        end--;
-        if (_sum < target) return 0;
-        _size = end-start+1;
-        ans = _size;
-
+        int _size=INT32_MAX;
+        int start=0, end=0, _sum=nums[end];
         while (end < nums.size()){
             if (_sum >= target){
-                ans = min(_size, ans);
-                _sum -= nums[start];
-                start++;
-                _size --;
+                _size = min(end-start+1, _size);
+                _sum -= nums[start++];
             }
             else {
                 end ++;
                 if (end < nums.size()){
                     _sum += nums[end];
-                    _size++;
                 }
             }
-
         }
-        return ans;
+        return _size == INT32_MAX ? 0 : _size;
     }
 };
 
 int main(){
-    // vector<int> nums{2,3,1,2,4,3}; 
-    // int target = 7;
-    vector<int> nums{2,16,14,15};
-    int target = 20;
+    vector<int> nums{2,3,1,2,4,3}; 
+    int target = 7;
+    // vector<int> nums{2,16,14,15};
+    // int target = 20;
     Solution s = Solution();
     double ans = s.minSubArrayLen(target, nums);
     cout << ans << "\n";
