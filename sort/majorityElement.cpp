9152ec74915a36bab9f5164f6341eed70a8c4ecf
SimonCao1207 <hainam@kaist.ac.kr>
2024-01-30 13:39:49 +0900

    im back

---
+++ sort/majorityElement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int appear=0, i=0, ans=-1;
+        int prev;
+        for (auto x: nums){
+            if (i==0) prev = x; 
+            if (prev == x) appear++;
+            else appear = 0; 
+            prev = x; 
+            i++;
+            if (appear >= nums.size()/2)
+                ans = x; 
+        }
+        return ans;     
+    }
+    
+};
+
+int main(){
+    // vector<int> nums{3,2,3};
+    vector<int> nums{2,2,1,1,1,2,2};
+    Solution s = Solution();
+    int ans = s.majorityElement(nums);
+    cout << "Ans : " << ans << "\n";
+}
